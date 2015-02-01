0d1a3f30be2fc6f1518b89ab2ef5aec1422e2563
Holly Negley <holly.negley@icloud.com>
2015-01-31 22:41:43 -0500

    created object preseidents

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var presidents[
+    
+    {firstName: "Barack",
+     lastName: "Obama",
+     termLength: "2",
+     party: "Democrat", 
+     yearsOfPresidency: "6"}, 
+
+    {firstName: "George W.", 
+     lastName: "Bush", 
+     termLength: "2", 
+     party: "Republican", 
+     yearsOfPresidency: "8"}, 
+    
+    {firstName: "Bill", 
+     lastName: "Clinton", 
+     termLength: "2", 
+     party: "Democrat", 
+     yearsOfPresidency: "8"},
+    
+    {firstName: "George H. W.", 
+     lastName: "Bush", 
+     termLength: "2", 
+     party: "Republican", 
+     yearsOfPresidency: "8"},
+    
+    {firstName: "Ronald", 
+     lastName: "Reagan", 
+     termLength: "2", 
+     party: "Republican", 
+     yearsOfPresidency: "8"}
+    
+]
+
+console.log(presidents ["Bill", "CLinton"]);
