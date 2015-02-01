9750ec89cb1d16aeb411df82daf3d051bbb454e2
Holly Negley <holly.negley@icloud.com>
2015-01-31 23:46:32 -0500

    created 3 functions

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function howdy() {
+    console.log("Holly Negley");
+} 
+
+howdy();
+
+
+
+
+var numba = function(string)
+
+if (string < 7) {console.log ("What a short word!"); }
+
+else (string > 7) {console.log ("I'm sorry, but that's too many to count.");}
+
+else if (string === 7){console.log ("7, what a perfect choice."); }
+    
+    
+
+
+function inception(callback,value){
+    callback(value);
+}
+    
+function value(){
+    console.log("Mud is a fantastic movie")
+}
+
+inception(value);
