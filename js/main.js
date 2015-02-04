37ea8e56a6a2adeccc387e1632d5e4355af6674e
Richard Kalehoff <richardkalehoff@gmail.com>
2015-02-03 20:59:45 -0500

    fix spacing for readability

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
 function howdy() {
     console.log("Holly Negley");
-} 
+}
 
 howdy();
 
 
-
-
 var numba = function(string) {
-
-if (string < 7) {console.log ("What a short word!"); }
-
-else if (string > 7) {console.log ("I'm sorry, but that's too many to count.");}
-
-else if (string === 7){console.log ("7, what a perfect choice."); }
-    
+    if (string < 7) {
+        console.log("What a short word!");
+    } else if (string > 7) {
+        console.log("I'm sorry, but that's too many to count.");
+    } else if (string === 7) {
+        console.log("7, what a perfect choice.");
+    }
 }
 
 
-function inception(callback,value){
+function inception(callback, value) {
     callback(value);
 }
-    
-function value(){
+
+function value() {
     console.log("Mud is a fantastic movie")
 }
 
